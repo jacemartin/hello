5e648044db87df8d6e780bc82ab03f99e8abbb7f
jacemartin <joanna.martin@modirum.com>
2014-10-28 09:48:16 +0200

    retain 1 test

--- src/test/java/com/java/ex/hello/AllTests.java
+++ src/test/java/com/java/ex/hello/AllTests.java
@@ -5,7 +5,7 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({ WebDriverDemoShootoutTest.class, WebDriverTest.class,
+@SuiteClasses({ //WebDriverDemoShootoutTest.class, WebDriverTest.class,
 		WebDriverWithHelperTest.class })
 public class AllTests {
 
