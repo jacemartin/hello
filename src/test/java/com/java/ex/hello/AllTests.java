3849d1407db8b624188704187523ff4d968cf75e
jacemartin <joanna.martin@modirum.com>
2014-10-29 09:15:32 +0200

    added comment

--- src/test/java/com/java/ex/hello/AllTests.java
+++ src/test/java/com/java/ex/hello/AllTests.java
@@ -3,7 +3,7 @@ package com.java.ex.hello;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
-
+//test
 @RunWith(Suite.class)
 @SuiteClasses({ //*WebDriverDemoShootoutTest.class, WebDriverTest.class,
 		WebDriverWithHelperTest.class })
